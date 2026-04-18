480090098adce923399f7d2cda85f1d8c00407b5
suis setiawan <cemet@MacBooks-Suis.local>
2026-04-19 01:52:41 +0700

    feat: configure CORS policy in SecurityConfig to allow cross-origin requests from specified origins

--- src/main/java/com/dibimbing/foodorder/config/SecurityConfig.java
+++ src/main/java/com/dibimbing/foodorder/config/SecurityConfig.java
@@ -58,7 +58,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8084", "http://localhost:8080"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:8084",
+                "http://localhost:8080",
+                "https://demo2.suissetiawan.my.id"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
